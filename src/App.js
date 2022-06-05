4abe5bd231f56bc50f4b1a749b2fc72aa262e2a0
JeremiahJacob261 <jeremiahjacob261@gmail.com>
2022-06-05 13:25:25 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './style.css';
 import Mac from './Mac';
 
 export default function App() {
+  state = {
+    count = 0
+  }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <button>show me</button>
+      <button onClick ={click}>{count}</button>
       <Mac name="jerry" />
     </div>
   );
+}
+function click(){
+setState=({
+  count = this.state.count +1
+})
 }
